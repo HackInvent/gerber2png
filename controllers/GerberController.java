5153961debe833155d357a1f5847d392d2a03131
HackInvent <HackInvent@users.noreply.github.com>
2016-08-10 16:28:52 +0200

    Update GerberController.java

--- controllers/GerberController.java
+++ controllers/GerberController.java
@@ -2,5 +2,14 @@ package com.hackinvent.play.gerber.controllers;
 
 class GerberController {
 
+  Result getGerber2png(){
+    
+    return ok("TBD");
+  }
+  
+  Result getGerberSize(){
+    
+    return ok("TBD");
+  }
 
 };
